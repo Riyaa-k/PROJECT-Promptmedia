b1826a610e1164e338e6ed643059093dde553fb1
Riyaa-k <anshitakoshta28@gmail.com>
2024-01-24 20:32:02 +0530

    Finall

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-  title: "PromptMedia",
+  title: "Prompt🔍Media",
   description: "Discover & Share AI Prompts",
 };
 
